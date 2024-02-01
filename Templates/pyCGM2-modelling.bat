2c94fb5fab3e13467cdfdb0bd9cf0ed7ac742e20
Fabien Leboeuf <aaa34169@users.noreply.github.com>
2024-02-01 14:29:42 +0100

    new pyCGM2 interactions

---
+++ Templates/pyCGM2-modelling.bat
@@ -0,0 +1,2 @@
+call C:\Users\fleboeuf\Miniconda3\Scripts\activate.bat pycgm39
+call pyCGM2.exe QTM CGM Modelling    
---
+++ Templates/pyCGM2-processing.bat
@@ -0,0 +1,3 @@
+call C:\Users\fleboeuf\Miniconda3\Scripts\activate.bat pycgm39
+call pyCGM2.exe QTM CGM Processing    
+
