28240c811c66dc6e4a2570ad89e0cd9465646a4f
Fabien Leboeuf <aaa34169@users.noreply.github.com>
2024-02-23 15:05:11 +0100

    bat file improved

--- Templates/pyCGM2-modelling.bat
+++ Templates/pyCGM2-modelling.bat
@@ -1,2 +1,8 @@
-call C:\Users\fleboeuf\Miniconda3\Scripts\activate.bat pycgm39
+rem Set the path to your Miniconda installation directory
+set "CONDA_PATH=C:\Users\fleboeuf\Miniconda3"
+
+rem Set the name of your virtual environment
+set "ENV_NAME=pycgm39"
+
+call "%CONDA_PATH%\Scripts\activate.bat" %ENV_NAME%
 call pyCGM2.exe QTM CGM Modelling    
